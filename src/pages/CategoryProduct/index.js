bf838d0a5bfda38a4d7a133f1c66f2d6b36d6cd5
Chinh Lâm Điền <lamdienchinhtd9a2@gmail.com>
2022-11-16 14:32:36 +0700

    modified CategoryProduct

--- src/pages/CategoryProduct/index.js
+++ src/pages/CategoryProduct/index.js
@@ -5,88 +5,36 @@ const cx = classNames.bind(styles);
 function CategoryProduct() {
   return (
     <div className={cx('wrapper')}>
-      <div className={cx('body')}>
-        <div className={cx('col1')}>
-          <div className={cx('list-item')}>
-            <ul>
-              <li><a>Nike</a></li>
-              <li><a>Converse</a></li>
-              <li><a>Puma</a></li>
-              <li><a>Adidas</a></li>
-            </ul>
-          </div>
-        </div>
-        <div className={cx('col2')}>
-          <div className={cx('row')}>
-            <div className={cx('item')}>
-              {<Product new sale='30' size={false} URL_img='https://ordixi.com/wp-content/uploads/2020/10/0eb4c83d.jpg'
-                name='Sneaker class 1 ' price_new='594,300đ' price_old='849,000đ' />}
-            </div>
-            <div className={cx('item')}>
-              {<Product new sale='30' size={false} URL_img='https://ordixi.com/wp-content/uploads/2020/10/0eb4c83d.jpg'
-                name='Sneaker class 1 ' price_new='594,300đ' price_old='849,000đ' />}
-            </div>
-            <div className={cx('item')}>
-              {<Product new sale='30' size={false} URL_img='https://ordixi.com/wp-content/uploads/2020/10/0eb4c83d.jpg'
-                name='Sneaker class 1 ' price_new='594,300đ' price_old='849,000đ' />}
-            </div>
-            <div className={cx('item')}>
-              {<Product new sale='30' size={false} URL_img='https://ordixi.com/wp-content/uploads/2020/10/0eb4c83d.jpg'
-                name='Sneaker class 1 ' price_new='594,300đ' price_old='849,000đ' />}
+      <div class="grid">
+        <div className={cx('body')}>
+          <div className={cx('col1', '.grid__column-25')}>
+            <div className={cx('list-item')}>
+              <ul>
+                <li><a>Nike</a></li>
+                <li><a>Converse</a></li>
+                <li><a>Puma</a></li>
+                <li><a>Adidas</a></li>
+              </ul>
             </div>
           </div>
-          <div className={cx('row')}>
-            <div className={cx('item')}>
-              {<Product new sale='30' size={false} URL_img='https://ordixi.com/wp-content/uploads/2020/10/0eb4c83d.jpg'
-                name='Sneaker class 1 ' price_new='594,300đ' price_old='849,000đ' />}
-            </div>
-            <div className={cx('item')}>
-              {<Product new sale='30' size={false} URL_img='https://ordixi.com/wp-content/uploads/2020/10/0eb4c83d.jpg'
-                name='Sneaker class 1 ' price_new='594,300đ' price_old='849,000đ' />}
-            </div>
-            <div className={cx('item')}>
-              {<Product new sale='30' size={false} URL_img='https://ordixi.com/wp-content/uploads/2020/10/0eb4c83d.jpg'
-                name='Sneaker class 1 ' price_new='594,300đ' price_old='849,000đ' />}
-            </div>
-            <div className={cx('item')}>
-              {<Product new sale='30' size={false} URL_img='https://ordixi.com/wp-content/uploads/2020/10/0eb4c83d.jpg'
-                name='Sneaker class 1 ' price_new='594,300đ' price_old='849,000đ' />}
-            </div>
-          </div>
-          <div className={cx('row')}>
-            <div className={cx('item')}>
-              {<Product new sale='30' size={false} URL_img='https://ordixi.com/wp-content/uploads/2020/10/0eb4c83d.jpg'
-                name='Sneaker class 1 ' price_new='594,300đ' price_old='849,000đ' />}
-            </div>
-            <div className={cx('item')}>
-              {<Product new sale='30' size={false} URL_img='https://ordixi.com/wp-content/uploads/2020/10/0eb4c83d.jpg'
-                name='Sneaker class 1 ' price_new='594,300đ' price_old='849,000đ' />}
-            </div>
-            <div className={cx('item')}>
-              {<Product new sale='30' size={false} URL_img='https://ordixi.com/wp-content/uploads/2020/10/0eb4c83d.jpg'
-                name='Sneaker class 1 ' price_new='594,300đ' price_old='849,000đ' />}
-            </div>
-            <div className={cx('item')}>
-              {<Product new sale='30' size={false} URL_img='https://ordixi.com/wp-content/uploads/2020/10/0eb4c83d.jpg'
-                name='Sneaker class 1 ' price_new='594,300đ' price_old='849,000đ' />}
-            </div>
-          </div>
-          <div className={cx('row')}>
-            <div className={cx('item')}>
-              {<Product new sale='30' size={false} URL_img='https://ordixi.com/wp-content/uploads/2020/10/0eb4c83d.jpg'
-                name='Sneaker class 1 ' price_new='594,300đ' price_old='849,000đ' />}
-            </div>
-            <div className={cx('item')}>
-              {<Product new sale='30' size={false} URL_img='https://ordixi.com/wp-content/uploads/2020/10/0eb4c83d.jpg'
-                name='Sneaker class 1 ' price_new='594,300đ' price_old='849,000đ' />}
-            </div>
-            <div className={cx('item')}>
-              {<Product new sale='30' size={false} URL_img='https://ordixi.com/wp-content/uploads/2020/10/0eb4c83d.jpg'
-                name='Sneaker class 1 ' price_new='594,300đ' price_old='849,000đ' />}
-            </div>
-            <div className={cx('item')}>
-              {<Product new sale='30' size={false} URL_img='https://ordixi.com/wp-content/uploads/2020/10/0eb4c83d.jpg'
-                name='Sneaker class 1 ' price_new='594,300đ' price_old='849,000đ' />}
+          <div className={cx('col2', '.grid__column-75')}>
+            <div className={cx('row')}>
+              <div className={cx('grid__column-25')}>
+                {<Product new={false} size={false} sale='30' URL_img='https://cf.shopee.vn/file/000519dcca77fb2c399bc1473b8ece60'
+                  name='Dây giày Nike 1' price_new='70,000đ' price_old='100,000đ' />}
+              </div>
+              <div className={cx('grid__column-25')}>
+                {<Product new={false} sale='30' size={false} URL_img='https://cf.shopee.vn/file/000519dcca77fb2c399bc1473b8ece60'
+                  name='Dây giày Nike 1' price_new='70,000đ' price_old='100,000đ' />}
+              </div>
+              <div className={cx('grid__column-25')}>
+                {<Product new={false} sale='30' size={false} URL_img='https://cf.shopee.vn/file/000519dcca77fb2c399bc1473b8ece60'
+                  name='Dây giày Nike 1' price_new='70,000đ' price_old='100,000đ' />}
+              </div>
+              <div className={cx('grid__column-25')}>
+                {<Product new={false} sale='30' size={false} URL_img='https://cf.shopee.vn/file/000519dcca77fb2c399bc1473b8ece60'
+                  name='Dây giày Nike 1' price_new='70,000đ' price_old='100,000đ' />}
+              </div>
             </div>
           </div>
         </div>
